38f7c25b17a6c5c32bf1f98c942b6185d984df60
j13012 <j13012@sangi.jp>
2017-02-10 13:33:41 +0900

    commit ButtonEvent
    Completed

--- ButtonEvent/ViewController.swift
+++ ButtonEvent/ViewController.swift
@@ -9,17 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var btn: UIButton!
+    @IBOutlet weak var label:UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let buttonWidth: CGFloat = 200
+        let buttonHeight: CGFloat = 40
+        let posX: CGFloat = (self.view.bounds.width - buttonWidth)/2
+        let posY: CGFloat = (self.view.bounds.height - buttonHeight)/2
+        btn.frame = CGRect(x: posX, y: posY, width: buttonWidth, height: buttonHeight)
+        btn.addTarget(self, action: #selector(buttonTapped(sender:)), for: .touchDown)
+        self.view.addSubview(btn)
     }
-
+    
+    
+    @IBAction func buttonTapped(sender: UIButton) {
+        
+        // UIAlertController
+        let alertController: UIAlertController = UIAlertController(title: "クリック", message: "ボタンをクリックしました！", preferredStyle: .alert)
+        
+        // 選択肢
+        // 異なる方法でactionを設定してみた
+        let actionOK = UIAlertAction(title: "OK", style: .default){
+            action in
+            print("OK")
+        }
+        
+        // actionを追加
+        alertController.addAction(actionOK)
+        
+        // UIAlertの起動
+        present(alertController, animated: true, completion: nil)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
-
